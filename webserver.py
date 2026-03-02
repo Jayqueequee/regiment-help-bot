747470ecd9414bbc4b23f60d9c8632ff5553f317
Jayqueequee <145878359+Jayqueequee@users.noreply.github.com>
2026-03-01 22:24:02 -0600

    Refactor webserver.py to include threading

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from threading import Thread
 
 app = Flask('')
 @app.route('/')
@@ -11,4 +12,5 @@ def run():
     
 def keep_alive():
     t = Thread(target=run)
+
     t.start()
